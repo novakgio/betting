d65b2c57619bacd0b730b76a5dc1001cadb3fd11
novaknole <nkura14@freeuni.edu.ge>
2017-07-29 12:18:12 +0400

    blabla

--- resources/views/pages/today_pick.blade.php
+++ resources/views/pages/today_pick.blade.php
@@ -9,7 +9,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <title>WinnerTips</title>
 
-
+<link rel="stylesheet" href="public/css/grid.css">
+<link rel="stylesheet" href="public/css/style_table.css">
 @include('urls_include.css_urls')
 
 </head>
@@ -35,6 +36,83 @@
 @include('layouts_include.header')
 
 
+<div class="section" style="height: 100vh; background: url(public/images/todayspick.jpg); background-position: center; background-repeat: no-repeat; background-size: cover;">
+
+  <div class="container my-container">
+    <h1 class="my-h1">TODAYS PICK</h1>
+    <div class-"my-cont-child" style="width: 100%;">
+        <div class="row dark-block">
+            <div class="col span-1-of-6">
+                <h6 style="color: white; margin-top: 17px;">1</h6>
+            </div>
+            <div class="col span-3-of-6">
+                <div class="row">
+                    <div class="col span-1-of-3">
+                        <h5 class="table-h5 right-aligned">FC RED XPRESS</h5>
+                    </div>
+                    <div class="col span-1-of-3">
+                        <h4 class="table-h4">VS</h5>
+                    </div>
+                    <div class="col span-1-of-3">
+                        <h5 class="table-h5 left-aligned">FC RED XPRESS</h5>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col span-1-of-3 right-aligned">
+                        <span class="first-span">AUGUST 24,2015</span>
+                    </div>
+                    <div class="col span-1-of-3 center-aligned">
+                        <span class="middle-span">18.25 PM</span>
+                    </div>
+                    <div class="col span-1-of-3 left-aligned">
+                        <span class="third-span">Antigua Recreation</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col span-2-of-6">
+                <div class="centerd-container">
+                    <button class="my-btn-red">SOLD OUT</button>
+                </div>
+            </div>
+        </div>
+        <div class="row light-block">
+            <div class="col span-1-of-6">
+                <h6 style="color: white; margin-top: 17px;">1</h6>
+            </div>
+            <div class="col span-3-of-6">
+                <div class="row">
+                    <div class="col span-1-of-3">
+                        <h5 class="table-h5 right-aligned">FC RED XPRESS</h5>
+                    </div>
+                    <div class="col span-1-of-3">
+                        <h4 class="table-h4">VS</h5>
+                    </div>
+                    <div class="col span-1-of-3">
+                        <h5 class="table-h5 left-aligned">FC RED XPRESS</h5>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col span-1-of-3 right-aligned">
+                        <span class="first-span">AUGUST 24,2015</span>
+                    </div>
+                    <div class="col span-1-of-3 center-aligned">
+                        <span class="middle-span">18.25 PM</span>
+                    </div>
+                    <div class="col span-1-of-3 left-aligned">
+                        <span class="third-span">Antigua Recreation</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col span-2-of-6">
+                <div class="centerd-container">
+                    <button class="my-btn-green">SOLD OUT</button>
+                </div>
+            </div>
+        </div>
+      </div>
+  </div>
+
+</div>
 
 <!--=================================
  inner-intro -->
